d7d95bbd833d674e0026208cfee439b617a8d113
Sang-Hyeok Yang <55566364+yy-ssang@users.noreply.github.com>
2021-03-25 09:36:28 +0900

    Create models.py

---
+++ model/models.py
@@ -0,0 +1,2 @@
+print('Model is available after publishing paper.')
+print("The model's structure is based on paper (Zhang, Kai and Li, Yawei and Zuo, Wangmeng and Zhang, Lei and Van Gool, Luc and Timofte, Radu, Plug-and-Play Image Restoration with Deep Denoiser Prior, arXiv preprint, 2020)")
